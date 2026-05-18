f2ea1b430711cfc2afedb722ea6c8fe93f841508
mortebrume <me@mortebrume.eu>
2026-05-18 11:31:03 +0200

    chore: remove unused vendor keyboards

--- examples/display/rules.mk
+++
@@ -1,5 +0,0 @@
-# Include the display.c file during compilation
-SRC += display.c
-
-# Include the font file
-SRC += graphics/thintel15.qff.c
--- keyboards/splitkb/aurora/helix/keymaps/default_hlc/rules.mk
+++
@@ -1,5 +0,0 @@
-ENCODER_MAP_ENABLE = yes
-
-USER_NAME := halcyon_modules
-CONVERT_TO = halcyon
-SRC += halcyon_keys.c
